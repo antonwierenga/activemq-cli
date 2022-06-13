563e2c0609d22b4f54c946ce068dae6a604d038a
antonwierenga <anton@antonwierenga.com>
2022-06-13 21:21:03 +0200

    Revert "artemis jmx domain"
    This reverts commit 4f550caf

--- src/main/scala/activemq/cli/ActiveMQCLI.scala
+++ src/main/scala/activemq/cli/ActiveMQCLI.scala
@@ -38,9 +38,7 @@ class ActiveMQCLI extends CommandMarker {
 
 object ActiveMQCLI extends App {
 
-  lazy val ReleaseNotes = Map("v0.9.1" → List(
-    "Support for artemis jmx domain"
-  ), "v0.9.0" → List(
+  lazy val ReleaseNotes = Map("v0.9.0" → List(
     "Updated shell command 'list-queues': new option --exclude-filter",
     "Updated shell command 'purge-all-queues': new option --exclude-filter",
     "Updated shell command 'remove-all-queues': new option --exclude-filter"
--- src/main/scala/activemq/cli/command/Commands.scala
+++ src/main/scala/activemq/cli/command/Commands.scala
@@ -156,17 +156,16 @@ abstract class Commands extends PrintStackTraceExecutionProcessor {
               )
               jmxConnector.connect
               // Fuse ESB Enterprise 7.1.0 / ActiveMQ 5.9.0 use different ObjectNames
-              val brokerViewMBeans = List("org.apache.activemq", "org.apache.activemq.artemis").par.map(broker ⇒
-                List(Map("type" → "type", "brokerName" → "brokerName"), Map("type" → "Type", "brokerName" → "BrokerName"))
-                  .par.map(properties ⇒
-                    jmxConnector.getMBeanServerConnection.queryNames(
-                      new ObjectName(s"${broker}:${properties.get("type").get}=Broker,${properties.get("brokerName").get}=${matched.jmxName.getOrElse("*")}"), //scalastyle:ignore
-                      null //scalastyle:ignore
-                    )).flatten
-                  .par.map(objectName ⇒ MBeanServerInvocationHandler.newProxyInstance(
-                    jmxConnector.getMBeanServerConnection, objectName, classOf[BrokerViewMBean], true
-                  ))
-                  .filter(!_.isSlave)).flatten
+              val brokerViewMBeans = List(Map("type" → "type", "brokerName" → "brokerName"), Map("type" → "Type", "brokerName" → "BrokerName"))
+                .par.map(properties ⇒
+                  jmxConnector.getMBeanServerConnection.queryNames(
+                    new ObjectName(s"org.apache.activemq:${properties.get("type").get}=Broker,${properties.get("brokerName").get}=${matched.jmxName.getOrElse("*")}"), //scalastyle:ignore
+                    null //scalastyle:ignore
+                  )).flatten
+                .par.map(objectName ⇒ MBeanServerInvocationHandler.newProxyInstance(
+                  jmxConnector.getMBeanServerConnection, objectName, classOf[BrokerViewMBean], true
+                ))
+                .filter(!_.isSlave)
 
               brokerViewMBeans.headOption match {
                 case Some(brokerViewMBean) ⇒ {
